55cbce6a12ff4bd1cf899a7d3593cb2b64168eb4
Faruq <faruqabdulsalam@yahoo.com>
2021-08-18 21:28:16 +0100

    Add form submission functionality

--- page/views.py
+++ page/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import ContactForm
 
 # Create your views here.
+
+def my_resume_view(request):
+
+    
+    if request.method == "POST": 
+        if "contact" in request.POST: 
+            name = request.POST["name"] 
+            email = request.POST["email"] 
+            message = request.POST["message"] 
+
+            form = ContactForm(name=name, email=email, message=message)
+            print(form)
+            form.save()
+
+    context = {
+    }
+
+    return render(request, 'resume.html', context)
