ceb3553b1fddd1a0b5ff70792ceb40fba74e7ca7
Faruq <faruqabdulsalam@yahoo.com>
2021-10-01 10:53:38 +0100

    Add email sending functionality

--- page/views.py
+++ page/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import ContactForm
 
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 # Create your views here.
 
 def my_resume_view(request):
@@ -10,13 +14,32 @@ def my_resume_view(request):
         if "contact" in request.POST: 
             name = request.POST["name"] 
             email = request.POST["email"] 
+            subjectt = request.POST["subject"] 
             message = request.POST["message"] 
 
             form = ContactForm(name=name, email=email, message=message)
-            print(form)
             form.save()
+
+            subject = subjectt
+            sender = 'Email from ' + name
+            body= {
+               'name' : 'name: ' + name ,
+                'email' : 'email: ' + email ,
+                'message' : 'message: ' + message,
+            }
+            message = '\n'.join(body.values())
+            mail_from= settings.EMAIL_HOST_USER
+            mail_to= ['faruqabdulsalam10@gmail.com']
+            send_mail(
+                    subject,
+                    message,
+                    mail_from,
+                    mail_to,
+                    fail_silently=False,
+                )
 
     context = {
     }
 
     return render(request, 'resume.html', context)
+
--- resume/settings.py
+++ resume/settings.py
@@ -132,4 +132,13 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#Send Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'resumemail17@gmail.com'
+EMAIL_HOST_PASSWORD = 'resmail123' 
+
 django_heroku.settings(locals())
+
