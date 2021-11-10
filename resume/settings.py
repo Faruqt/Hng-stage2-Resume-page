13f75c5e009ee87fb8355c1423fb9248be895f1a
Faruq <faruqabdulsalam@yahoo.com>
2021-11-10 17:20:24 +0100

    Update foodies link

--- resume/settings.py
+++ resume/settings.py
@@ -134,8 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Send Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'resumemail17@gmail.com'
 EMAIL_HOST_PASSWORD = 'resmail123' 
