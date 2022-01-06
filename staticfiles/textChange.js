59583d1084bec0a7a1a34210b9d933e8516900da
Faruq <faruqabdulsalam@yahoo.com>
2022-01-06 22:43:05 +0100

    Add new projects

--- staticfiles/textChange.js
+++ staticfiles/textChange.js
@@ -1,7 +1,7 @@
 //Replace Text function		 
 $(function () { 
     count = 0; 
-    wordsArray = ["Web Developer", "Electrical and Electronics Engineer", " Embedded Systems Software Engineer",  "Anime Lover"]; 
+    wordsArray = ["Web Developer", "Technical Writer", "Electrical and Electronics Engineer", " Embedded Systems Software Engineer",  "Anime Lover"]; 
     setInterval(function () { 
       count++; 
       $("#word").fadeOut(500, function () { 
